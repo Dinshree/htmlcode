a92bdad0c7b95c4f70b5a791c41c788356ba72eb
Dinshree <dinshree82@gmail.com>
2025-07-21 12:13:48 +0530

    directives code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Directives';
+  isDisplay:boolean=true;
+buttonName='Hide';
+
+changeDisplayValue(){
+ this.isDisplay=!this.isDisplay;
+ if(this.isDisplay) {
+  this.buttonName="Hide";
+ } else{
+  this.buttonName="Show";
+ }
+}
+//for
+rolls=[1,2,3,4,5]
+
+student:{roll:number,name:string}[]=[
+  // {roll:101, name:"aa"},
+  //  {roll:102, name:"aa"},
+  //   {roll:103, name:"aa"},
+]
+
+num=2;
+one(){
+  this.num=1;
+}
+two(){
+  this.num=2;
+}
 }
